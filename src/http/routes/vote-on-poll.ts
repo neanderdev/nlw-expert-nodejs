f03e71e07e6e7254964734eabd38a482ed5d95dc
Neander de Souza <neander.faria@hotmail.com>
2024-02-19 16:33:13 -0300

    Aula 03

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -3,6 +3,9 @@ import { randomUUID } from "node:crypto";
 import { z } from "zod";
 
 import { prisma } from "../../lib/prisma";
+import { redis } from "../../lib/redis";
+
+import { voting } from "../../utils/voting-pub-sub";
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post("/polls/:pollId/votes", async (request, reply) => {
@@ -38,6 +41,17 @@ export async function voteOnPoll(app: FastifyInstance) {
             id: userPreviousVoteOnPoll.id,
           },
         });
+
+        const votes = await redis.zincrby(
+          pollId,
+          -1,
+          userPreviousVoteOnPoll.pollOptionId
+        );
+
+        voting.publish(pollId, {
+          pollOptionId: userPreviousVoteOnPoll.pollOptionId,
+          votes: Number(votes),
+        });
       } else if (userPreviousVoteOnPoll) {
         return reply
           .status(400)
@@ -62,6 +76,13 @@ export async function voteOnPoll(app: FastifyInstance) {
         pollId,
         pollOptionId,
       },
+    });
+
+    const votes = await redis.zincrby(pollId, 1, pollOptionId);
+
+    voting.publish(pollId, {
+      pollOptionId,
+      votes: Number(votes),
     });
 
     return reply.status(201).send();
